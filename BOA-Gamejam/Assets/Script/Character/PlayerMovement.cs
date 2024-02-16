f5f9618383091d14b828c59af00aac971619ce2a
nefeygt <n.efeyigit@outlook.com>
2024-02-17 01:18:00 +0300

    player idle anim

---
+++ BOA-Gamejam/Assets/Script/Character/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Vector2 direction;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        TakeInput();
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        SetAnimatorMovement(direction);
+    }
+
+    private void TakeInput()
+    {
+        direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+    }
+
+    private void SetAnimatorMovement(Vector2 direction)
+    {
+        animator.SetFloat("xDir", direction.x);
+        animator.SetFloat("yDir", direction.y);
+    }
+}
