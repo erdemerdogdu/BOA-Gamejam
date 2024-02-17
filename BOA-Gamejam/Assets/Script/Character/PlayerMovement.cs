401c4fcb7de492d67700fb8a7b1f70655ec564c6
nefeygt <n.efeyigit@outlook.com>
2024-02-17 21:45:12 +0300

    Revert "Merge branch 'main' of https://github.com/bariscihanoglu/BOA-Gamejam"
    This reverts commit 6d69ccbddcd800f83aaf8b90cf10f7cc601c9b14, reversing
    changes made to 6fcb50b9757f01e400fbe74e2540f86f8478cb3f.

--- BOA-Gamejam/Assets/Script/Character/PlayerMovement.cs
+++ BOA-Gamejam/Assets/Script/Character/PlayerMovement.cs
@@ -1,4 +1,3 @@
-using JetBrains.Annotations;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,6 +6,64 @@ using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
+    /* public float speed = 5f;
+    private Vector2 direction;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        TakeInput();
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if (direction.x != 0 || direction.y != 0)
+        {
+            SetAnimatorMovement(direction);
+        }
+        else
+        {
+            animator.SetLayerWeight(1, 0);
+        }
+    }
+
+    private void TakeInput()
+    {
+        direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+    }
+
+    private void SetAnimatorMovement(Vector2 direction)
+    {
+        animator.SetLayerWeight(1, 1);
+        animator.SetFloat("xDir", direction.x);
+        animator.SetFloat("yDir", direction.y);
+    } */
+
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
@@ -16,12 +73,20 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        movement = movement.normalized;
-
+        movement.y = Input.GetAxisRaw(("Vertical"));
+        
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        /* if (animator.speed > 0.01)
+        {
+            animator.SetLayerWeight(1, 1);
+        }
+        else
+        {
+            animator.SetLayerWeight(1, 0);
+        } */
     }
 
     void FixedUpdate()
