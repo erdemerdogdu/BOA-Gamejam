6b8e60bbf4c124074ab6c7ba1155653beb5b5c54
Barış Cihanoğlu <bariscihanoglu4@gmail.com>
2024-02-18 01:36:06 +0300

    Update Init.cs

--- BOA-Gamejam/Assets/Init.cs
+++ BOA-Gamejam/Assets/Init.cs
@@ -7,6 +7,6 @@ public class Init : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        gameObject.GetComponent<ViewManager>().PlayScene("Menu");
+        //gameObject.GetComponent<ViewManager>().PlayScene("Menu");
     }
 }
