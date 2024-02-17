379a37af89979dfa506e4249157367e3a92da420
nefeygt <n.efeyigit@outlook.com>
2024-02-17 13:20:14 +0300

    movement animator changed ama not done yet

--- BOA-Gamejam/Assets/Script/Character/PlayerMovement.cs
+++ BOA-Gamejam/Assets/Script/Character/PlayerMovement.cs
@@ -6,7 +6,7 @@ using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
+    /* public float speed = 5f;
     private Vector2 direction;
     private Animator animator;
 
@@ -24,7 +24,15 @@ public class PlayerMovement : MonoBehaviour
     private void Move()
     {
         transform.Translate(direction * speed * Time.deltaTime);
-        SetAnimatorMovement(direction);
+
+        if (direction.x != 0 || direction.y != 0)
+        {
+            SetAnimatorMovement(direction);
+        }
+        else
+        {
+            animator.SetLayerWeight(1, 0);
+        }
     }
 
     private void TakeInput()
@@ -51,7 +59,29 @@ public class PlayerMovement : MonoBehaviour
 
     private void SetAnimatorMovement(Vector2 direction)
     {
+        animator.SetLayerWeight(1, 1);
         animator.SetFloat("xDir", direction.x);
         animator.SetFloat("yDir", direction.y);
+    } */
+
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+    
+    Vector2 movement;
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw(("Vertical"));
+        
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
